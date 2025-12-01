b800be2a88a738eabb91ed928f943795dfd8f72d
Kevin Sinn <kevinsinn01@gmail.com>
2025-12-01 11:55:07 +0100

    seving first static page

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
 	serveMux := http.NewServeMux()
+	serveMux.Handle("/", http.FileServer(http.Dir("../../web")))
 
 	server := &http.Server{
 		Handler: serveMux,
 		Addr:    ":8080",
 	}
 
+	fmt.Println("Server is running on port 8080")
 	server.ListenAndServe()
 }
