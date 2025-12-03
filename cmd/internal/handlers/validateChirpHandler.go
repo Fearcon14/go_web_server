543f82fe72d2038c1de93fd9055adaeba8d60ddd
Kevin Sinn <kevinsinn01@gmail.com>
2025-12-03 13:06:02 +0100

    helper functions to reduce repeat code

--- cmd/internal/handlers/validateChirpHandler.go
+++ cmd/internal/handlers/validateChirpHandler.go
@@ -6,48 +6,52 @@ import (
 	"encoding/json"
 )
 
-func ValidateChirpHandler(w http.ResponseWriter, r *http.Request) {
-	type ChirpRequest struct {
-		Body string `json:"body"`
-	}
+type ChirpResponse struct {
+	Error string `json:"error"`
+	Valid bool   `json:"valid"`
+}
 
-	type ChirpResponse struct {
-		Error string `json:"error"`
-		Valid bool   `json:"valid"`
-	}
+type ChirpRequest struct {
+	Body string `json:"body"`
+}
+
+func ValidateChirpHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	decoder := json.NewDecoder(r.Body)
 	var chirpRequest ChirpRequest
 	err := decoder.Decode(&chirpRequest)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		data, err := json.Marshal(ChirpResponse{Error: "Invalid request body", Valid: false})
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.Write(data)
+		respondWithError(w, http.StatusBadRequest, "Invalid request body")
 		return
 	}
 
 	if len(chirpRequest.Body) > 140 {
-		w.WriteHeader(http.StatusBadRequest)
-		data, err := json.Marshal(ChirpResponse{Error: "Chirp is too long", Valid: false})
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.Write(data)
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
 		return
 	}
 
 	w.WriteHeader(http.StatusOK)
-	data, err := json.Marshal(ChirpResponse{Error: "", Valid: true})
+	respondWithJSON(w, http.StatusOK, ChirpResponse{Error: "", Valid: true})
+
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	data, err := json.Marshal(ChirpResponse{Error: message, Valid: false})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	w.WriteHeader(code)
 	w.Write(data)
+}
 
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(data)
 }
